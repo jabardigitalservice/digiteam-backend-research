2f2f46fe2c99ce187f6a41795c5568764c99f2e2
Michael Liondy <mike@liondy.com>
2023-03-07 19:42:52 +0700

    fix: docker entrypoint script

--- sd-without-library/docker/docker-entrypoint.sh
+++ sd-without-library/docker/docker-entrypoint.sh
@@ -7,8 +7,9 @@ env=${APP_ENV:-production}
 
 if [ "$env" != "local" ]; then
     echo "Setup configuration..."
-    cat laravel_app_config > /var/www/html/.env
-    cat run/secrets/laravel_app_secrets >> /var/www/html/.env
+    ls -la
+    cat /laravel_app_config > /var/www/html/.env
+    cat /run/secrets/laravel_app_secrets >> /var/www/html/.env
     echo "Caching configuration..."
     php /var/www/html/artisan optimize
 fi

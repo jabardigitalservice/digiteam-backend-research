b72c7d4342b1d10deb4d4119a5ceeba133e528a4
Michael Liondy <mike@liondy.com>
2023-03-07 19:15:16 +0700

    feat: try adding setup configuration in server

--- sd-without-library/docker/docker-entrypoint.sh
+++ sd-without-library/docker/docker-entrypoint.sh
@@ -6,6 +6,9 @@ role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
 
 if [ "$env" != "local" ]; then
+    echo "Setup configuration..."
+    cat laravel_app_config > /var/www/html/.env
+    cat run/secrets/laravel_app_secrets >> /var/www/html/.env
     echo "Caching configuration..."
     php /var/www/html/artisan optimize
 fi
